df32095f2317058bcb35b565f8ed90254033b449
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-04-13 19:53:47 -0700

    Create POST delete endpoint and add delete button to urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -42,6 +42,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
