2eecf5bb96a88c625da44b442e763b836e1e6928
Kurt Galvin <kurtgalvin@gmail.com>
2020-04-17 01:12:41 +0000

    Fix POST /login and POST /register bugs

--- express_server.js
+++ express_server.js
@@ -54,10 +54,16 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  const templateVars = {
-    user: users[req.session.user_id]
+  const { user_id } = req.session;
+  const user = users[user_id]
+  if (!user) {
+    const templateVars = {
+      user
+    }
+    res.render("register", templateVars)
+  } else {
+    res.redirect("/urls")
   }
-  res.render("register", templateVars)
 })
 
 app.post("/register", (req, res) => {
@@ -66,28 +72,33 @@ app.post("/register", (req, res) => {
     res.status(400).send("Must include Email and Password")
   } else if (getUserByEmail(email, users)) {
     res.status(400).send("Email already in use")
+  } else {
+    const id = uid(6)
+    users[id] = { id, email, password: bcrypt.hashSync(password, 10) }
+    req.session.user_id = id
+    res.redirect("/urls")
   }
-  const id = uid(6)
-  users[id] = { id, email, password: bcrypt.hashSync(password, 10) }
-  req.session.user_id = id
-  res.redirect("/urls")
-
 })
 
 app.get("/login", (req, res) => {
-  const templateVars = {
-    user: users[req.session.user_id]
+  const { user_id } = req.session;
+  const user = users[user_id]
+  if (!user) {
+    const templateVars = {
+      user
+    }
+    res.render("login", templateVars)
+  } else {
+    res.redirect("/urls")
   }
-  res.render("login", templateVars)
 })
 
 app.post("/login", (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
+  const user = getUserByEmail(email, users);
   if (!email || !password) {
     res.status(400).send("Must include Email and Password");
-  }
-  const user = getUserByEmail(email, users);
-  if (user && bcrypt.compareSync(password, user.password)) {
+  } else if (user && bcrypt.compareSync(password, user.password)) {
     req.session.user_id = user.id
     res.redirect("/urls")
   } else {
